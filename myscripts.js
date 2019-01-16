c67d37d59a86597aba1cb31cd09bdac9452bc35a
Jesus Lara <40151140+jlara-11@users.noreply.github.com>
2019-01-16 16:34:23 -0600

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,7 @@
+//This is where we will make an automatic slideshow:
+
+// When the user clicks on the button, scroll to the top of the document (topbtn):
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
